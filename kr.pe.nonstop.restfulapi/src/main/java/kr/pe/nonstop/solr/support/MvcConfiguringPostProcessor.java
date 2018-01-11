00a9b7f4f8232d51c7ff828b194ceeb605d5e41a
netcrazy <ilkyu.choi@gmail.com>
2018-01-11 22:56:09 +0900

    spring 4.x upgrade

--- kr.pe.nonstop.restfulapi/src/main/java/kr/pe/nonstop/solr/support/MvcConfiguringPostProcessor.java
+++ kr.pe.nonstop.restfulapi/src/main/java/kr/pe/nonstop/solr/support/MvcConfiguringPostProcessor.java
@@ -4,7 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 /**
 * The HTTP Message converters are created automatically by Spring. To perform
@@ -15,15 +15,15 @@ public class MvcConfiguringPostProcessor implements BeanPostProcessor {
 
 	/**
 	* Enable pretty print on any bean of type
-	* {@link MappingJacksonHttpMessageConverter} or
+	* {@link MappingJackson2HttpMessageConverter} or
 	* {@link MappingJackson2HttpMessageConverter}.
 	*/
 	public Object postProcessBeforeInitialization(Object bean, String name) throws BeansException 
 	{
 		if (bean instanceof HttpMessageConverter<?>)
 		
-		if (bean instanceof MappingJacksonHttpMessageConverter) {	
-			((MappingJacksonHttpMessageConverter) bean).setPrettyPrint(true);
+		if (bean instanceof MappingJackson2HttpMessageConverter) {
+			((MappingJackson2HttpMessageConverter) bean).setPrettyPrint(true);
 		} 
 		else if (bean instanceof MappingJackson2HttpMessageConverter) {
 			((MappingJackson2HttpMessageConverter) bean).setPrettyPrint(true);
